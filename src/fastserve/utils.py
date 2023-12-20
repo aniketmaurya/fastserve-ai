12487357bad8c9221b0d127c6ad99eaa6cd025ca
Aniket Maurya <theaniketmaurya@gmail.com>
2023-12-20 23:39:40 +0000

    fix dep

--- src/fastserve/utils.py
+++ src/fastserve/utils.py
@@ -1,10 +1,11 @@
 from typing import Any
 
-import torch
 from pydantic import BaseModel
 
 
 def get_default_device():
+    import torch
+
     if torch.cuda.is_available():
         return "cuda"
     if torch.backends.mps.is_available():
