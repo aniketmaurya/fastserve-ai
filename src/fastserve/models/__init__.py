ff6a211cd1e037cca7292d26ba04c791028b2a55
Aniket Maurya <theaniketmaurya@gmail.com>
2023-12-21 00:01:51 +0000

    fixes

---
+++ src/fastserve/models/__init__.py
@@ -0,0 +1,3 @@
+from fastserve.models.llama_cpp import ServeLlamaCpp as ServeLlamaCpp
+from fastserve.models.sdxl_turbo import ServeSDXLTurbo as ServeSDXLTurbo
+from fastserve.models.ssd import ServeSSD1B as ServeSSD1B
