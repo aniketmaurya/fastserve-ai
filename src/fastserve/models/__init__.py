4807f743f26930868e3f7deaad5b88ce204e58b0
Aniket Maurya <theaniketmaurya@gmail.com>
2023-12-28 21:12:41 +0000

    add image classification (#11)

--- src/fastserve/models/__init__.py
+++ src/fastserve/models/__init__.py
@@ -1,4 +1,7 @@
 from fastserve.models.face_reco import FaceDetection as FaceDetection
+from fastserve.models.image_classification import (
+    ServeImageClassification as ServeImageClassification,
+)
 from fastserve.models.llama_cpp import ServeLlamaCpp as ServeLlamaCpp
 from fastserve.models.sdxl_turbo import ServeSDXLTurbo as ServeSDXLTurbo
 from fastserve.models.ssd import ServeSSD1B as ServeSSD1B
