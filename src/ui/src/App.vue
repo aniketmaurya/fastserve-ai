cae99bc266075e38590db333d6cde4308939b02b
Aniket Maurya <theaniketmaurya@gmail.com>
2024-02-15 13:38:25 +0000

    npm project

---
+++ src/ui/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ImageCard from './components/ImageCard.vue';
+
+</script>
+
+<template>
+  <h1 class="flex justify-center text-3xl font-bold underline">
+    SDXL Turbo
+  </h1>
+  <ImageCard/>
+</template>
+
+<style>
+</style>
