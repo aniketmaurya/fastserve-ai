deab8d3f4e1eefffd6619072ff4342bfd34f2626
Aniket Maurya <aniketmaurya@outlook.com>
2024-02-23 00:45:55 +0000

    update

--- src/fastserve/models/__init__.py
+++ src/fastserve/models/__init__.py
@@ -5,3 +5,4 @@ from fastserve.models.image_classification import (
 from fastserve.models.llama_cpp import ServeLlamaCpp as ServeLlamaCpp
 from fastserve.models.sdxl_turbo import ServeSDXLTurbo as ServeSDXLTurbo
 from fastserve.models.ssd import ServeSSD1B as ServeSSD1B
+from fastserve.models.vllm import ServeVLLM as ServeVLLM
--- src/fastserve/models/vllm.py
+++ src/fastserve/models/vllm.py
@@ -2,19 +2,18 @@ import logging
 import os
 from typing import Any, List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from fastserve.core import FastServe
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_MODEL = "openhermes-2-mistral-7b.Q6_K.gguf"
 
 
 class PromptRequest(BaseModel):
-    prompt: str = "Llamas are cute animal"
+    prompt: str = "Write a python function to resize image to 224x224"
     temperature: float = 0.8
-    top_p: float = 0.0
+    top_p: float = 1.0
     max_tokens: int = 200
     stop: List[str] = []
 
@@ -29,7 +28,7 @@ class ResponseModel(BaseModel):
 class ServeVLLM(FastServe):
     def __init__(
         self,
-        model_path=DEFAULT_MODEL,
+        model,
         batch_size=1,
         timeout=0.0,
         *args,
@@ -37,42 +36,31 @@ class ServeVLLM(FastServe):
     ):
         from vllm import LLM
 
-        if not os.path.exists(model_path):
-            raise FileNotFoundError(f"{model_path} not found.")
-
-        self.llm = LLM(model_path)
-        self.model_path = model_path
+        self.llm = LLM(model)
         self.args = args
         self.kwargs = kwargs
         super().__init__(
             batch_size,
             timeout,
             input_schema=PromptRequest,
-            response_schema=ResponseModel,
+            # response_schema=ResponseModel,
         )
 
     def __call__(self, request: PromptRequest) -> Any:
         from vllm import SamplingParams
 
         sampling_params = SamplingParams(
-            temperature=request.temperature, top_p=request.top_p
+            temperature=request.temperature, top_p=request.top_p,
+            max_tokens=request.max_tokens
         )
-        result = self.llm(request.prompt, sampling_params=sampling_params)
+        result = self.llm.generate(request.prompt, sampling_params=sampling_params)
         logger.info(result)
         return result
 
-    def handle(self, batch: List[PromptRequest]) -> List[ResponseModel]:
+    def handle(self, batch: List[PromptRequest]) -> List:
         responses = []
         for request in batch:
             output = self(request)
-
-            response = ResponseModel(
-                **{
-                    "prompt": request.prompt,
-                    "text": output["choices"][0]["text"],
-                    "finished": True,
-                }
-            )
-            responses.append(response)
+            responses.extend(output)
 
         return responses
