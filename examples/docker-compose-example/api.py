5554b65530e0afeda85814f52d15d1546510abf9
Carlos André Antunes <c4rlo4@gmail.com>
2024-03-01 14:17:40 -0300

    Feature/server containerization (#26)
    * example of face recognition using docker-compose
    
    * updating README: adding docker section and run_server explanation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ examples/docker-compose-example/api.py
@@ -0,0 +1,11 @@
+import os
+
+from fastserve.models import FaceDetection
+
+# example of getting environment variables setted in docker-compose.yml
+batch_size = os.environ.get("BATCH_SIZE", 2)
+timeout = os.environ.get("TIMEOUT", 1)
+
+
+app = FaceDetection(batch_size=batch_size, timeout=timeout)
+app.run_server(host="0.0.0.0")
