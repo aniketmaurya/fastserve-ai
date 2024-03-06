ee9c245a1812c862f87ee0b04315b829fe2f015b
Antunes <c4rlo4@gmail.com>
2024-03-06 19:38:48 -0300

    fixing merge bug

--- src/fastserve/models/__init__.py
+++ src/fastserve/models/__init__.py
@@ -1,6 +1,5 @@
 from fastserve.models.face_reco import FaceDetection as FaceDetection
 from fastserve.models.huggingface import ServeHuggingFace as ServeHuggingFace
-from fastserve.models.huggingface import ServeHuggingface as ServeHuggingface
 from fastserve.models.image_classification import (
     ServeImageClassification as ServeImageClassification,
 )
