cb5732aec28f471aa1798a70de20d364cd176aab
Antunes <c4rlo4@gmail.com>
2024-03-06 15:11:37 -0300

    adding huggingface support

--- src/fastserve/models/__main__.py
+++ src/fastserve/models/__main__.py
@@ -5,6 +5,7 @@ from fastserve.models.face_reco import FaceDetection
 from fastserve.models.llama_cpp import ServeLlamaCpp
 from fastserve.models.sdxl_turbo import ServeSDXLTurbo
 from fastserve.models.ssd import ServeSSD1B
+from fastserve.models.huggingface import ServeHuggingface
 from fastserve.utils import get_default_device
 
 parser = argparse.ArgumentParser(description="Serve models with FastServe")
@@ -69,6 +70,13 @@ elif args.model == "llama-cpp":
     )
 elif args.model == "face-detection":
     app = FaceDetection(
+        timeout=args.timeout,
+        batch_size=args.batch_size,
+    )
+elif args.model == "huggingface":
+    app = ServeHuggingface(
+        model_name=args.model_name,
+        device=device,
         timeout=args.timeout,
         batch_size=args.batch_size,
     )
