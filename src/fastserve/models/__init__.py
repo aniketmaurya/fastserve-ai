f1088ee7b5bd3de7c4ac143c271780152cd4fd4e
Aniket Maurya <theaniketmaurya@gmail.com>
2024-03-19 16:52:47 +0000

    Add new models and update imports

--- src/fastserve/models/__init__.py
+++ src/fastserve/models/__init__.py
@@ -1,9 +1,9 @@
-from fastserve.models.face_reco import FaceDetection as FaceDetection
-from fastserve.models.huggingface import ServeHuggingFace as ServeHuggingFace
-from fastserve.models.image_classification import (
+from fastserve.models.cv.face_reco import FaceDetection as FaceDetection
+from fastserve.models.llm.huggingface import ServeHuggingFace as ServeHuggingFace
+from fastserve.models.cv.image_classification import (
     ServeImageClassification as ServeImageClassification,
 )
-from fastserve.models.llama_cpp import ServeLlamaCpp as ServeLlamaCpp
-from fastserve.models.sdxl_turbo import ServeSDXLTurbo as ServeSDXLTurbo
+from fastserve.models.llm.llama_cpp import ServeLlamaCpp as ServeLlamaCpp
+from fastserve.models.image_gen.sdxl_turbo import ServeSDXLTurbo as ServeSDXLTurbo
 from fastserve.models.ssd import ServeSSD1B as ServeSSD1B
-from fastserve.models.vllm import ServeVLLM as ServeVLLM
+from fastserve.models.llm.vllm import ServeVLLM as ServeVLLM
